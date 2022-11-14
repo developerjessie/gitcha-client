920aeb8741a31e6e3657c840cc071c318cdf0574
developerjessie <99792713+developerjessie@users.noreply.github.com>
2022-11-14 21:17:20 +0900

    chore: add webpack dev settings and prod settings

---
+++ webpack.dev.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: false,
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    compress: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
